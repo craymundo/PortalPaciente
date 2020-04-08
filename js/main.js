d8cd6e808d7cbe62b2a529e479bd157a3b109f2c
calvarador <cesar_alva17@hotmail.com>
2020-04-08 17:28:09 -0500

    registro de usuario

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
 $(document).ready(function() {
     var height = $(window).height();
     $(".content_login_right").height(height);
-
-
 });
 
 function activar_paso(id) {
-    console.log('PASO:' + id);
     if (id == '1') {
         $('#paso_2').fadeOut(500, function() {
             $('#paso_1').fadeIn();
             $("#lblSubTitulo").parent().show();
+
             $("#lblSubTitulo").text("Datos Personales");
             $("#lblSubTituloPrincipal").text("Registrar mis datos");
 
-            $(".timeline.text-center").parent().show();
-
-            $("body > div > div.content_login_right > div").attr('style', 'width: 360px!important');
+            $(".timeline.text-center").parent().removeClass("d-none");
+            $("body > div > div.content_login_right > div").removeClass("contenedor_form_registro_w2");
+            $("body > div > div.content_login_right > div").addClass("contenedor_form_registro_w1");
         });
     } else if (id == '2') {
 
         $('#paso_1').fadeOut(500, function() {
             $('#paso_2').fadeIn();
             $("#lblSubTitulo").parent().show();
+
             $("#lblSubTitulo").text("Datos de contacto");
             $("#lblSubTituloPrincipal").text("Registrar mis datos");
-            $(".timeline.text-center").parent().show();
-            //
-            $("body > div > div.content_login_right > div").attr('style', 'width: 360px!important');
+
+            $(".timeline.text-center").parent().removeClass("d-none");
+            $("body > div > div.content_login_right > div").removeClass("contenedor_form_registro_w2");
+            $("body > div > div.content_login_right > div").addClass("contenedor_form_registro_w1");
         });
     } else if (id == '3') {
 
         $('#paso_2').fadeOut(500, function() {
             $('#terminos_y_condiciones').fadeIn();
             $("#lblSubTitulo").parent().hide();
-            $("#lblSubTituloPrincipal").text("Términos y condiciones");
-            $("#lblSubTituloPrincipal").addClass("mt-titulo-terminos");
-            $(".timeline.text-center").parent().attr('style', 'display: none !important');
 
-            $("body > div > div.content_login_right > div").attr('style', 'width: 504px!important');
+            $("#lblSubTituloPrincipal").text("Términos y condiciones");
+
+            $("#lblSubTituloPrincipal").addClass("mt-titulo-terminos");
+            $(".timeline.text-center").parent().addClass("d-none");
+            $("body > div > div.content_login_right > div").removeClass("contenedor_form_registro_w1");
+            $("body > div > div.content_login_right > div").addClass("contenedor_form_registro_w2");
         });
     } else if (id == '4') {
 
@@ -46,13 +48,36 @@ function activar_paso(id) {
         $('#terminos_y_condiciones').fadeOut(500, function() {
             $('#paso_2').fadeIn();
             $("#lblSubTitulo").parent().show();
+
             $("#lblSubTitulo").text("Datos de contacto");
             $("#lblSubTituloPrincipal").text("Registrar mis datos");
+
+            $(".timeline.text-center").parent().removeClass("d-none");
             $("#lblSubTituloPrincipal").removeClass("mt-titulo-terminos");
-            $(".timeline.text-center").parent().show();
-            //
-            $("body > div > div.content_login_right > div").attr('style', 'width: 360px!important');
+            $("body > div > div.content_login_right > div").removeClass("contenedor_form_registro_w2");
+            $("body > div > div.content_login_right > div").addClass("contenedor_form_registro_w1");
         });
 
+    } else if (id == '5') {
+
+
+        $('#terminos_y_condiciones').fadeOut(500, function() {
+            $('#paso_2').fadeIn();
+            $("#lblSubTitulo").parent().show();
+
+            $("#lblSubTitulo").text("Datos de contacto");
+            $("#lblSubTituloPrincipal").text("Registrar mis datos");
+
+            $(".timeline.text-center").parent().removeClass("d-none");
+            $("#lblSubTituloPrincipal").removeClass("mt-titulo-terminos");
+            $("body > div > div.content_login_right > div").removeClass("contenedor_form_registro_w2");
+            $("body > div > div.content_login_right > div").addClass("contenedor_form_registro_w1");
+        });
+
+    } else if (id == '6') {
+        $('#paso_2').fadeOut(500, function() {
+            $('#mensaje_sucess').fadeIn();
+
+        });
     }
 }
